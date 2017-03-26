f2a01abbba96259dafa4b5e071ae744baa77757c
HsHack <hshack@e1z3r1p20.42.us.org>
2017-03-25 20:15:12 -0700

    Initial Commit

---
+++ IngredientScanner/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IngredientScanner
+//
+//  Created by HsHack on 3/25/17.
+//  Copyright © 2017 HsHack. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
